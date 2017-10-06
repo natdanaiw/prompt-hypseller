6a1be72765ecf16c6eb8ffb347eef1c5bda468f2
STA-HQ Natdanai Witayasirikul <Natdanaiw@ad.sritranggroup.com>
2017-10-06 17:25:35 +0700

    Update resrouce for home screen icon.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://shq-nbitsw99:10000/';
+
 $config['base_url'] = 'http://localhost:10000/';
 //$config['base_url'] = 'https://www.hypseller.com/';
 
