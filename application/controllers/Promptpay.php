6718f6dc0f9fbd08578cc8eb937177b8b93e1265
unknown <natdanai.wit@gmail.com>
2017-11-02 00:32:47 +0700

    test

--- application/controllers/Promptpay.php
+++ application/controllers/Promptpay.php
@@ -14,7 +14,7 @@ class Promptpay extends CI_Controller {
 	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
-	 * So any other public methods not prefixed with an underscore will
+	 * So any other public methosd not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
